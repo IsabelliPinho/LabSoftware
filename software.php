3fafe10a54fdb2745f7a6cb59f70e1563260c16b
IsabelliPinho <108484363+IsabelliPinho@users.noreply.github.com>
2023-06-18 19:09:54 -0300

    Create software.php

---
+++ software.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html><head>
+  <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kurale&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lao+Muang+Khong&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Marmelad&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" /><title>Document</title></head><body>
+        <div class="v79_81"><div class="v79_82"></div><div class="v79_83"></div><div class="v79_84"></div>
+        <div class="v79_94"></div><div class="v79_85"></div><div class="v79_92"></div><div class="v79_88"><div class="v79_89"><div class="v79_90"></div><div class="v79_91"></div></div></div>
+        <span class="v79_86"><a href="">Tela Inicial</a></span>
+        <div class="v81_124"></div>
+        <span class="v79_95">Welcome to Health Life</span><div class="v79_96"></div>
+        
+           
+        
+            <!-- Inputs -->
+            <input type="text" class="v79_97" placeholder="username" required maxlength="12"></div>
+            <input type="text" class="v79_99" placeholder="..........." required maxlength="8">
+           
+
+            <!-- checkbox -->
+            <input type="checkbox" class="v79_102">
+            <span class="v79_1020">Lembrar?</span>
+        <span class="v79_104">Esqueceu a senha?</span><div class="v79_105"></div>
+           
+        <!-- Botões -->
+            <button class="v79_105">ENVIAR</button>
+             <button class="v114_53">CADASTRAR-SE</button>
+             
+             <!-- insta,twitter,face.linkedin -->
+             <div class="v79_107"></div>
+            <div class="v79_111"></div>
+            <div class="v79_115"></div>
+            <div class="v79_119"></div>
+          <!-- Background -->
+          <div class="v79_98"></div>
+            <div class="v81_125"></div>
+        <div class="v81_126"></div><div class="v81_128"></div><div class="v81_127"></div><div class="v81_129">
+
+        </div><div class="v81_133">
+        </div><div class="v81_131"></div><div class="v81_132"></div></div></body></html>
